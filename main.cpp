75331fa222d138f58c0c81767d4d287cdcf6da5c
Mayank Choudhary <44155446+mayank271@users.noreply.github.com>
2018-11-03 17:32:53 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include "Header.h"
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+    /**
+     * For taking input from command line.
+     */
+    string a;
+    cin >> a;
+    cout << input(a)[0] <<endl;
+    cout << input(a)[1];
+
+    /**
+     * For taking input from a file.
+     * Each instruction should be on a separate line
+     * in the specified file.
+     */
+
+    return 0;
+}
