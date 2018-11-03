188a6c9c62b6e6f5ffa116688f2cc38372b95853
Mayank Choudhary <44155446+mayank271@users.noreply.github.com>
2018-11-03 17:34:02 +0530

    Create MIPS_functions.cpp

---
+++ MIPS_functions.cpp
@@ -0,0 +1,33 @@
+#include <regex>
+#include <string>
+#include <vector>
+#include "Header.h"
+
+#define I_SIZE 8
+
+/**
+ * Takes input from the command line, validates it and then
+   disassembles it into the corresponding instruction,
+   R-type instruction : [ OPCODE ] [ RS ] [ RT ] [ RD ] [ SHMT ] [ FUNCT ]
+   I-type instruction : [ OPCODE ] [ RS ] [ RT ] [ IMMEDIATE ]
+   J-type instruction : [ OPCODE ] [ ADDRESS ]
+   Incase of invalid (doesn't match regex - [0-9a-f]{8}) instruction
+   it returns an empty vector.
+ * @param i : Input string from main
+ * @return vector containing two strings - instruction type and the decoded instruction.
+ */
+std::vector<std::string> input(std::string i){
+    std::vector<std::string> decoded;
+    std::regex e ("[0-9a-f]{8}");
+
+    if(!std::regex_match(i,e)){
+        //"Invalid Input";
+    }
+    else{
+        std::vector <int> temp = binaryVal(i);
+        std::vector <int> curr(temp.begin(),temp.begin()+6);
+        int op_code = bin_to_int(curr);
+        decoded = decode_opcode(op_code,temp);
+    }
+    return decoded;
+}
