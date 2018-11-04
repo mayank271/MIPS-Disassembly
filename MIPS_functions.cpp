b2ab8dbad3529b1ff0b4d20a55b8b7b4a86db652
Mayank Choudhary <44155446+mayank271@users.noreply.github.com>
2018-11-05 00:08:46 +0530

    Update MIPS_functions.cpp

--- MIPS_functions.cpp
+++ MIPS_functions.cpp
@@ -8,6 +8,172 @@
 //Other functions yet to be added
 
 /**
+ * Methods to convert data from one form to another.
+ */
+void int_to_bin(char c, std::vector<int> &binary){
+    /**
+     * Hardcoded char to binary conversion
+     * Returns nothing
+     */
+    switch (c){
+        case '0':{
+            binary.push_back(0);
+            binary.push_back(0);
+            binary.push_back(0);
+            binary.push_back(0);
+            break;
+        }
+        case '1':{
+            binary.push_back(0);
+            binary.push_back(0);
+            binary.push_back(0);
+            binary.push_back(1);
+            break;
+        }
+        case '2':{
+            binary.push_back(0);
+            binary.push_back(0);
+            binary.push_back(1);
+            binary.push_back(0);
+            break;
+        }
+        case '3':{
+            binary.push_back(0);
+            binary.push_back(0);
+            binary.push_back(1);
+            binary.push_back(1);
+            break;
+        }
+        case '4':{
+            binary.push_back(0);
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(0);
+            break;
+        }
+        case '5':{
+            binary.push_back(0);
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(1);
+            break;
+        }
+        case '6':{
+            binary.push_back(0);
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(0);
+            break;
+        }
+        case '7':{
+            binary.push_back(0);
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(1);
+            break;
+        }
+        case '8':{
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(0);
+            binary.push_back(0);
+            break;
+        }
+        case '9':{
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(0);
+            binary.push_back(1);
+            break;
+        }
+        case 'a':{
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(1);
+            binary.push_back(0);
+            break;
+        }
+        case 'A':{
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(1);
+            binary.push_back(0);
+            break;
+        }
+        case 'b':{
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(1);
+            binary.push_back(1);
+            break;
+        }
+        case 'B':{
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(1);
+            binary.push_back(1);
+            break;
+        }
+        case 'c':{
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(0);
+            break;
+        }
+        case 'C':{
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(0);
+            break;
+        }
+        case 'd':{
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(1);
+            break;
+        }
+        case 'D':{
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(0);
+            binary.push_back(1);
+            break;
+        }
+        case 'e':{
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(0);
+            break;
+        }
+        case 'E':{
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(0);
+            break;
+        }
+        case 'f':{
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(1);
+            break;
+        }
+        case 'F':{
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(1);
+            binary.push_back(1);
+            break;
+        }
+    }
+}
+
+/**
  * Takes input from the command line, validates it and then
    disassembles it into the corresponding instruction,
    R-type instruction : [ OPCODE ] [ RS ] [ RT ] [ RD ] [ SHMT ] [ FUNCT ]
