807e99c65a0ccc385008c68211c43f79807998b8
Mayank Choudhary <44155446+mayank271@users.noreply.github.com>
2018-11-05 00:11:14 +0530

    Update MIPS_functions.cpp
    Methods to interconvert data were added.

--- MIPS_functions.cpp
+++ MIPS_functions.cpp
@@ -172,7 +172,166 @@ void int_to_bin(char c, std::vector<int> &binary){
         }
     }
 }
+std::vector <int> binaryVal(std::string val){
+    /**
+     * Converts a string to its 32 bit representation
+     * Returns the integer vector with integers of value 0 or 1
+     */
+    std::vector<int> binary;
 
+    for(int i = 0; i < I_SIZE; i++){
+        int_to_bin(val[i],binary);
+    }
+
+
+    return binary;
+}
+int bin_to_int(std::vector<int> v){
+    /**
+     * Converts the given binary representation
+     * to its decimal representation.
+     */
+     int ans=0;
+     for(auto i = 0; i < v.size(); i++){
+         ans *= 2;
+         ans += v[i];
+     }
+
+     return ans;
+}
+std::string int_to_reg(int val){
+    switch(val){
+        case 0:{
+            return "$zero";
+        }
+        case 1:{
+            return "$at";
+        }
+        case 2:{
+            return "$v0";
+        }
+        case 3:{
+            return "$v1";
+        }
+        case 4:{
+            return "$a0";
+        }
+        case 5:{
+            return "$a1";
+        }
+        case 6:{
+            return "$a2";
+        }
+        case 7:{
+            return "$a3";
+        }
+        case 8:{
+            return "$t0";
+        }
+        case 9:{
+            return "$t1";
+        }
+        case 10:{
+            return "$t2";
+        }
+        case 11:{
+            return "$t3";
+        }
+        case 12:{
+            return "$t4";
+        }
+        case 13:{
+            return "$t5";
+        }
+        case 14:{
+            return "$t6";
+        }
+        case 15:{
+            return "$t7";
+        }
+        case 16:{
+            return "$s0";
+        }
+        case 17:{
+            return "$s1";
+        }
+        case 18:{
+            return "$s2";
+        }
+        case 19:{
+            return "$s3";
+        }
+        case 20:{
+            return "$s4";
+        }
+        case 21:{
+            return "$s5";
+        }
+        case 22:{
+            return "$s6";
+        }
+        case 23:{
+            return "$s7";
+        }
+        case 24:{
+            return "$t8";
+        }
+        case 25:{
+            return "$t9";
+        }
+        case 26:{
+            return "$k0";
+        }
+        case 27:{
+            return "$k1";
+        }
+        case 28:{
+            return "$gp";
+        }
+        case 29:{
+            return "$sp";
+        }
+        case 30:{
+            return "$fp";
+        }
+        case 31:{
+            return "$ra";
+        }
+
+    }
+}
+std::string int_to_freg(int val){
+    std::string ans = "$f";
+    ans += std::to_string(val);
+    return ans;
+}
+std::string to_hex(int val){
+    if(val<10){
+        return std::to_string(val);
+    }
+    else{
+        switch (val){
+            case 10:{
+                return "a";
+            }
+            case 11:{
+                return "b";
+            }
+            case 12:{
+                return "c";
+            }
+            case 13:{
+                return "d";
+            }
+            case 14:{
+                return "e";
+            }
+            case 15:{
+                return "f";
+            }
+        }
+    }
+}
 /**
  * Takes input from the command line, validates it and then
    disassembles it into the corresponding instruction,
